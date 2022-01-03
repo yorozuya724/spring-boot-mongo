1a740c7c9c2f875caa5e3b31a65380ce4a69c5ef
Hasan Ali AĞAR <hasanaliagar@Hasans-MacBook-Pro.local>
2022-01-03 12:21:17 +0300

    improved

--- student-service/dist/server.js
+++ student-service/dist/server.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Hello World! | 23'); //write a response to the client
+  res.write('Hello World! | 23 | 24'); //write a response to the client
   res.end(); //end the response
 }).listen(8080); //the server object listens on port 8080
